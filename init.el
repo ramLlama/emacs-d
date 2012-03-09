82570fa7a861b7c47529321356636897bfa69651
Ram Raghunathan <rraghunathan@tower-research.com>
2012-03-09 15:10:52 -0500

    Modified linux c mode
    1) Added linux-c-tabs-mode.el
    2) removed the wrong linux mode definition in init.el
    
    Change-Id: Ifdbe78aeef7b04b30e4bb08bc9fcc1447be94371

--- init.el
+++ init.el
@@ -156,19 +156,8 @@
 ;;
 ;; C, C++
 ;;
-(defun linux-c-mode ()
-  "C mode with adjusted defaults for use with the Linux kernel."
-  (interactive)
-  (c-mode)
-  (setq c-indent-level 8)
-  (setq c-brace-imaginary-offset 0)
-  (setq c-brace-offset -8)
-  (setq c-argdecl-indent 8)
-  (setq c-label-offset -8)
-  (setq c-continued-statement-offset 8)
-  (setq indent-tabs-mode nil)
-  (setq tab-width 8))
-(add-hook 'c-mode-common-hook 'google-make-newline-indent)
+;; linux c mode with tabs
+(require 'linux-c-tabs-mode)
 
 ;;
 ;; Eshell
