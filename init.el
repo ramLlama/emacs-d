b15ecef41baa317dbc10d1aacfb675d66d061315
Ram Raghunathan <rraghunathan@tower-research.com>
2012-09-19 10:32:27 -0400

    Load, but do not enable, Solarized themes
    Change-Id: I7a5bbf40ad1cc10bce71355bf7a6764a5475cfe9

--- init.el
+++ init.el
@@ -186,7 +186,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default))))
+)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -194,5 +194,6 @@
  ;; If there is more than one, they won't work right.
  '(highlight ((t (:background "black" :foreground "white")))))
 
-;; Set Solarized Dark Theme
-(load-theme 'solarized-dark t)
+;; Load Solarized themes without enabling
+(load-theme 'solarized-dark t t)
+(load-theme 'solarized-light t t)
