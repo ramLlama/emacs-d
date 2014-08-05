a307f8038326a25d063bbec409a753d8c8b61163
Ram Raghunathan <ram.r@cs.cmu.edu>
2014-08-05 16:29:07 -0400

    Set default font to Source Code Pro, 11pt

--- init.el
+++ init.el
@@ -399,3 +399,9 @@
  '(org-agenda-files
    (quote
     ("~/org/personal.org" "~/org/phd/dblab.org" "~/org/phd/parmem.org" "~/org/phd/parinc.org" "~/org/phd/biginc.org"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#002b36" :foreground "#839496" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :foundry "unknown" :family "Source Code Pro")))))
