c5df636f6f72fd6fa581c55a9509e321fa00d42b
Ram Raghunathan <ram.r@cs.cmu.edu>
2014-08-05 16:25:44 -0400

    Cleanup init.el

--- init.el
+++ init.el
@@ -393,9 +393,3 @@
  '(org-agenda-files
    (quote
     ("~/org/personal.org" "~/org/phd/dblab.org" "~/org/phd/parmem.org" "~/org/phd/parinc.org" "~/org/phd/biginc.org"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
