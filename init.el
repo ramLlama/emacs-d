ee6ca3b6f5c33622e379aaa64576814010a16301
Ram Raghunathan <ram.r@cs.cmu.edu>
2014-09-10 19:14:35 -0400

    Setup reftex

--- init.el
+++ init.el
@@ -409,9 +409,8 @@
  '(custom-safe-themes
    (quote
     ("f208be98a1816ec7b061ec70b80bfa3d5dde886bfb44d60832ca8d209bde5f5a" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" default)))
- '(org-agenda-files
-   (quote
-    ("~/org/personal.org" "~/org/phd/dblab.org" "~/org/phd/parmem.org" "~/org/phd/parinc.org" "~/org/phd/biginc.org"))))
+ '(org-agenda-files p)
+ '(reftex-ref-style-default-list (quote ("Varioref" "Hyperref" "Cleveref"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
