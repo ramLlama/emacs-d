3ccf95353ce8b59c5a0bcaad2140536884dbac51
Ram Raghunathan <ram@llama.is>
2014-10-23 08:45:54 -0400

    Fix auto-save directory

--- init.el
+++ init.el
@@ -47,19 +47,11 @@
 
 ;; Put autosave files (ie #foo#) in one place, *not*
 ;; scattered all over the file system!
-(defvar autosave-dir "~/.emacs.d/auto-save-dir/")
-
+(defconst auto-save-dir "~/.emacs.d/auto-save-dir/")
 (make-directory autosave-dir t)
+(setq auto-save-file-name-transforms
+      `((".*" ,auto-save-dir t)))
 
-(defun auto-save-file-name-p (filename)
-  (string-match "^#.*#$" (file-name-nondirectory filename)))
-
-(defun make-auto-save-file-name ()
-  (concat autosave-dir
-	  (if buffer-file-name
-	      (concat "#" (file-name-nondirectory buffer-file-name) "#")
-	    (expand-file-name
-	     (concat "#%" (buffer-name) "#")))))
 
 ;; Put backup files (ie foo~) in one place too. (The backup-directory-alist
 ;; list contains regexp=>directory mappings; filenames matching a regexp are
