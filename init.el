c82423c60053de940d188c88241f3dbefc135409
Ram Raghunathan <ram@llama.is>
2015-05-21 21:03:34 -0400

    init.el: update magit configuration

--- init.el
+++ init.el
@@ -140,6 +140,7 @@
 
 ;; magit
 (global-set-key (kbd "C-x C-z") 'magit-status)
+(setq magit-last-seen-setup-instructions "1.4.0")
 
 ;; goto-last-change
 (global-set-key (kbd "C-x /") 'goto-last-change)
