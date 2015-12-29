0c40f0ffc380e2243798d9557b314d6c3a0347c4
Ram Raghunathan <ram.r@cs.cmu.edu>
2015-12-29 10:57:27 -0500

    init.el: Consolidate org-mode settings

--- init.el
+++ init.el
@@ -294,27 +294,33 @@
 ;;
 ;; Org-mode
 ;;
+
+;; Org-Mode options
+(setq org-log-done 'time  ;; timestamp on completion
+      org-todo-keywords '((sequence
+                           "TODO(t)"
+                           "WIP(p!)"
+                           "WAITING(w!)"
+                           "|"
+                           "DONE(d)"
+                           "MISSED(m)"
+                           "PASSED-ON(a)"
+                           "DISCARDED(i)")) ;; list of todo states
+      org-startup-indented t ;; turn on org-indent-mode as I think in trees
+      org-use-speed-commands t ;; speed-keys are awesome!
+      org-enforce-todo-dependencies t ;; dependency management
+      org-agenda-dim-blocked-tasks t)
+
 ;; As I use org-mode for lists, use org-indent-mode and visual-line-mode
 (add-hook 'org-mode-hook
 	  (lambda ()
 	    (visual-line-mode 1)
 	    (flyspell-mode 1)))
 
-(setq org-log-done 'time  ;; timestamp on completion
-      org-todo-keywords '((sequence
-			   "TODO(t)"
-			   "WIP(p!)"
-			   "WAITING(w!)"
-			   "|"
-			   "DONE(d)"
-			   "MISSED(m)"
-			   "PASSED-ON(a)"
-			   "DISCARDED(i)")) ;; list of todo states
-      org-startup-indented t) ;; turn on org-indent-mode as I think in trees
-
-;; Set org-mode file associations
+;; keybindings
 (eval-after-load "org"
   (lambda ()
+    ;; File Associations
     ;; .txt files aren't in the list initially, but in case that changes
     ;; in a future version of org, use if to avoid errors
     (if (assoc "\\.pdf\\'" org-file-apps)
@@ -328,14 +334,9 @@
       (add-to-list 'org-file-apps '("\\.jpg\\'" . "firefox %s") t))
     (if (assoc "\\.jpeg\\'" org-file-apps)
 	(setcdr (assoc "\\.jpeg\\'" org-file-apps) "firefox %s")
-      (add-to-list 'org-file-apps '("\\.jpeg\\'" . "firefox %s") t))))
+    (add-to-list 'org-file-apps '("\\.jpeg\\'" . "firefox %s") t))
 
-;; Enable speed keys
-(setq org-use-speed-commands t)
-
-;; keybindings
-(eval-after-load "org"
-  (lambda ()
+    ;; Keybindings
     (org-defkey org-mode-map (kbd "C-c i") 'org-insert-heading)
     (org-defkey org-mode-map (kbd "C-c a") 'org-agenda)))
 
@@ -554,7 +555,7 @@
     ("31a01668c84d03862a970c471edbd377b2430868eccf5e8a9aec6831f1a0908d" "1297a022df4228b81bc0436230f211bad168a117282c20ddcba2db8c6a200743" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "f208be98a1816ec7b061ec70b80bfa3d5dde886bfb44d60832ca8d209bde5f5a" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" default)))
  '(org-agenda-files
    (quote
-    ("~/git/mine/org/personal.org" "~/git/mine/org/phd/dblab.org" "~/git/mine/org/phd/parmem.org" "~/git/mine/org/phd/parinc.org" "~/git/mine/org/phd/biginc.org")))
+    ("~/Dropbox/org/personal/personal.org" "~/Dropbox/org/phd/parmem.org")))
  '(reftex-ref-style-default-list (quote ("Varioref" "Hyperref" "Cleveref"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
