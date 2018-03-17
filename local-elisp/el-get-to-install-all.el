7ad63d73905fab1adc840d7ff417a5ea954148ab
Ram Raghunathan <ram@llama.is>
2018-03-17 00:12:02 +0000

    el-get-to-install-all: Update

--- local-elisp/el-get-to-install-all.el
+++ local-elisp/el-get-to-install-all.el
@@ -4,41 +4,46 @@
 
 (setq el-get-to-install
       '(el-get           ; el-get is self-hosting
-	switch-window    ; takes over C-x o
-	vkill            ; Process view and killing
-	buffer-move      ; move buffers through frames
-	smex             ; smart M-x
-	magit            ; git integration
-	goto-last-change ; what do you think?!
-	yasnippet        ; snippets
-	google-c-style
-	systemtap-mode
-	lua-mode
-	org-mode
-	emacs-color-theme-solarized
-	protobuf-mode
-	haskell-mode
-	auctex
-	auctex-latexmk
-	ag
-	lorem-ipsum
-	scss-mode
-	emacs-dirtree
-	web-mode
-	cperl-mode
-	adoc-mode
-	org-journal
-	sml-mode
-	esml-mlb-mode
-	fill-column-indicator
-	company-mode
-	tuareg-mode
-	ProofGeneral-cvs
-	rust-mode
-	dsvn
-	multiple-cursors
-	pdftools
-	irony-mode
-	irony-eldoc
-	company-irony
-	PlatformIO-Mode))
+        switch-window    ; takes over C-x o
+        vkill            ; Process view and killing
+        buffer-move      ; move buffers through frames
+        smex             ; smart M-x
+        magit            ; git integration
+        goto-last-change ; what do you think?!
+        yasnippet        ; snippets
+        google-c-style
+        systemtap-mode
+        lua-mode
+        org-mode
+        emacs-color-theme-solarized
+        protobuf-mode
+        haskell-mode
+        auctex
+        auctex-latexmk
+        ag
+        lorem-ipsum
+        scss-mode
+        emacs-dirtree
+        web-mode
+        cperl-mode
+        adoc-mode
+        org-journal
+        sml-mode
+        esml-mlb-mode
+        fill-column-indicator
+        company-mode
+        tuareg-mode
+        ProofGeneral-cvs
+        rust-mode
+        dsvn
+        multiple-cursors
+        pdftools
+        irony-mode
+        irony-eldoc
+        company-irony
+        PlatformIO-Mode
+        flycheck
+        flycheck-pos-tip
+        anaconda-mode
+        company-jedi
+        avy))
