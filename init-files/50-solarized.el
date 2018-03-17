31ea34f5631fe4e09111bcce44964c28e9db42b7
Ram Raghunathan <ram@llama.is>
2018-03-17 00:13:16 +0000

    50-solarized.el: Add commented out section for solarized-emacs

--- init-files/50-solarized.el
+++ init-files/50-solarized.el
@@ -2,3 +2,7 @@
 (load-theme 'solarized t)
 (setq frame-background-mode 'dark)
 (enable-theme 'solarized)
+
+;; ;; Load Solarized themes
+;; (load-theme 'solarized-dark t)
+;; (enable-theme 'solarized-dark)
