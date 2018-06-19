a76ff420153ed858cda2e479dd804bbdc44bcd8e
Ram Raghunathan <ram@llama.is>
2018-06-19 00:55:22 +0000

    init-files/irony: Make run better
    * Exclusively use libclang CDB loading
    * switch to eval-after-load instead of irony mode hook.
    * Add irony backend to flycheck.

--- init-files/50-irony.el
+++ init-files/50-irony.el
@@ -1,25 +1,22 @@
 ;;
 ;; irony
 ;;
-(add-hook 'irony-mode-hook
-          (lambda ()
-            (define-key irony-mode-map [remap completion-at-point]
-              'irony-completion-at-point-async)
+(with-eval-after-load 'irony-cdb
+  (setq irony-cdb-compilation-databases
+        (delete 'irony-cdb-json  irony-cdb-compilation-databases)))
+(with-eval-after-load 'irony
+  (define-key irony-mode-map [remap completion-at-point]
+    'irony-completion-at-point-async)
+  (define-key irony-mode-map [remap complete-symbol]
+    'irony-completion-at-point-async))
+(add-hook 'irony-mode-hook (lambda () (irony-cdb-autosetup-compile-options)))
 
-            (define-key irony-mode-map [remap complete-symbol]
-              'irony-completion-at-point-async)
-
-            (irony-cdb-autosetup-compile-options)))
-
-;; add to flycheck
+;; hooks into other modes
 (add-hook 'flycheck-mode-hook 'flycheck-irony-setup)
-
+(eval-after-load "company" '(add-to-list 'company-backends 'company-irony))
 (add-hook 'c-mode-hook (lambda ()
-                           (irony-mode)
-                           (irony-eldoc)))
-
-;; Enable irony for all c++ files, and platformio-mode only
-;; when needed (platformio.ini present in project root).
+                         (irony-mode)
+                         (irony-eldoc)))
 (add-hook 'c++-mode-hook (lambda ()
                            (irony-mode)
                            (irony-eldoc)))
