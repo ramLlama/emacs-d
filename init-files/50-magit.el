39f814f6ce8ca4f26fae313a87d64ffd5c21c2a4
Ram Raghunathan <ram@llama.is>
2018-06-19 00:54:19 +0000

    init-files/magit: Spellcheck commits.

--- init-files/50-magit.el
+++ init-files/50-magit.el
@@ -1,3 +1,5 @@
 ;; magit
 (global-set-key (kbd "C-x C-z") 'magit-status)
 (setq magit-last-seen-setup-instructions "1.4.0")
+(with-eval-after-load 'git-commit-mode
+  (add-to-list 'git-commit-mode-hook 'flyspell-mode))
