b47d876e097a69fcea3dd476af623cb99d33e7d8
Ram Raghunathan <ram@llama.is>
2018-06-19 00:56:54 +0000

    init-files/flycheck: Only check on save and 1.5s idle.
    This will prevent checks when just reading files or otherwise not
    doing stuff that needs flychecking.

--- init-files/50-flycheck.el
+++ init-files/50-flycheck.el
@@ -1,2 +1,4 @@
 (add-hook 'after-init-hook #'global-flycheck-mode)
 (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
+(setq-default flycheck-check-syntax-automatically '(save idle-change))
+(setq-default flycheck-idle-change-delay 1.5)
