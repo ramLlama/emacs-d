3c3afe1d1b3f9545ccb9aa9605f2cad2b1579fcd
Ram Raghunathan <ram@llama.is>
2018-11-28 23:11:23 +0000

    init.el: Add Melpa

--- init.el
+++ init.el
@@ -13,6 +13,21 @@
 (add-to-list 'load-path "~/.emacs.d/local-elisp.private")
 (add-to-list 'custom-theme-load-path "~/.emacs.d/local-elisp/themes")
 
+;;;;;;;;;;;
+;; Melpa ;;
+;;;;;;;;;;;
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
+(package-initialize)
+
 ;;;;;;;;;;;;
 ;; el-get ;;
 ;;;;;;;;;;;;
@@ -35,11 +50,6 @@
 
 (el-get 'sync el-get-to-install)
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
 
 ;;
 ;; Load from init.d
