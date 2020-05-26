3ddc0ffa702734448e68d965098be7efe3b7b709
Ram Raghunathan <ram@llama.is>
2020-05-26 16:40:12 -0700

    init-files/ws: Properly honor local fill-column.

--- init-files/50-whitespace.el
+++ init-files/50-whitespace.el
@@ -1,8 +1,6 @@
 (require 'whitespace)
 
 (setq whitespace-style '(face trailing lines empty tabs tab-mark))
-(setq whitespace-line-column nil)
-
 (face-spec-set 'whitespace-empty
                '((t (:background "black" :foreground "red"))))
 (face-spec-set 'whitespace-line
@@ -11,9 +9,21 @@
                '((t (:background "black"
                                  :foreground "magenta"
                                  :underline (:color foreground-color
-                                                      :style wave)))))
+                                                    :style wave)))))
 (face-spec-set 'whitespace-trailing
-                 '((t (:background "black"
-                                   :foreground "red"
-                                   :underline t
-                                   :weight bold))))
+               '((t (:background "black"
+                                 :foreground "red"
+                                 :underline t
+                                 :weight bold))))
+
+
+;; start whitespace-mode for all modes but _after_ the other
+;; hooks have run, including the ones that set 'fill-column.
+(add-hook 'after-change-major-mode-hook
+          (lambda ()
+            (progn
+              ;; from https://www.emacswiki.org/emacs/WhiteSpace to
+              ;; support buffer-local fill-column
+              (setq-local whitespace-line-column fill-column)
+              (when (or (derived-mode-p 'prog-mode) (derived-mode-p 'text-mode))
+                (whitespace-mode 1)))))
