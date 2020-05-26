ed8d881725bd0303bee431cce8f7aebed8eaa472
Ram Raghunathan <ram@llama.is>
2020-05-26 16:39:47 -0700

    init-files/sml: Cleanup.

--- init-files/50-sml.el
+++ init-files/50-sml.el
@@ -1,10 +1,13 @@
 ;;
 ;; SML
 ;;
-;; sml-mode turns indent-tabs-mode back on for some reason...
-(add-hook 'sml-mode-hook (lambda ()
-			   (setq indent-tabs-mode nil
-				 sml-indent-level 2
-                                 fill-column 100)))
 
+
+(require 'sml-mode)
+(add-hook 'sml-mode-hook (lambda ()
+                           (setq
+                            ;; sml-mode turns indent-tabs-mode back on for some reason...
+                            indent-tabs-mode nil
+                            sml-indent-level 2
+                            fill-column 100)))
 (add-to-list 'auto-mode-alist '("\\.fun\\'" . sml-mode))
