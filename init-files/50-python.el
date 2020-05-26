cf4641b9dce90755cc115afe8895900b113e4a08
Ram Raghunathan <ram@llama.is>
2020-05-26 16:40:01 -0700

    init-files/python: Remove extra variables.

--- init-files/50-python.el
+++ init-files/50-python.el
@@ -1,8 +1,3 @@
 (add-hook 'python-mode-hook
           (lambda ()
-            (progn (setq fill-column 100)
-                   (make-local-variable 'whitespace-line-column)
-                   (setq whitespace-line-column 98)
-                   ;; reload whitespace-mode
-                   (whitespace-mode -1)
-                   (whitespace-mode 1))))
+            (progn (setq fill-column 100))))
