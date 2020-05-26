21c4a29cc74b795edabed4a455a4b2ff5f32465a
Ram Raghunathan <ram@llama.is>
2020-05-26 14:15:10 -0700

    init-files/50-sml.el: Set fill-column to 100.

--- init-files/50-sml.el
+++ init-files/50-sml.el
@@ -4,6 +4,7 @@
 ;; sml-mode turns indent-tabs-mode back on for some reason...
 (add-hook 'sml-mode-hook (lambda ()
 			   (setq indent-tabs-mode nil
-				 sml-indent-level 2)))
+				 sml-indent-level 2
+                                 fill-column 100)))
 
 (add-to-list 'auto-mode-alist '("\\.fun\\'" . sml-mode))
