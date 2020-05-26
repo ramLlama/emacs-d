1fa31ce01d482c5b51909897976e1340e0196fe1
Ram Raghunathan <ram@llama.is>
2020-05-26 16:40:33 -0700

    init-files/prog-mode: Remove defunct options.

--- init-files/50-prog-mode.el
+++
@@ -1,9 +0,0 @@
-;;
-;; Common for all programming modes
-;;
-(defun prog-mode-setup ()
-  (flyspell-prog-mode)
-  (column-number-mode 1)
-  (whitespace-mode 1))
-
-(add-hook 'prog-mode-hook 'prog-mode-setup)
