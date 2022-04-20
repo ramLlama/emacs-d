a39fe40cb72307698296838cd3bdb74deddc83d9
Ram Raghunathan <rrrghun@twitch.tv>
2022-04-19 17:38:57 -0700

    python: run black on save.

--- init-files/50-python.el
+++ init-files/50-python.el
@@ -1,3 +1,6 @@
+(require 'python-black)
+
 (add-hook 'python-mode-hook
           (lambda ()
-            (progn (setq fill-column 100))))
+            (progn (setq fill-column 100)
+                   (python-black-on-save-mode))))
