90631585e0bbc99f353506d9a545fb001d280c23
Ram Raghunathan <ram@llama.is>
2022-06-01 21:26:12 -0700

    init-files: rename mesta to imli.

--- init-files/50-imli.el
+++ init-files/50-imli.el
@@ -1,8 +1,8 @@
-(add-to-list 'auto-mode-alist '("\\.mt\\'" . mesta-mode))
+(add-to-list 'auto-mode-alist '("\\.im\\'" . imli-mode))
 
-(eval-after-load 'mesta-mode
+(eval-after-load 'imli-mode
   '(progn
-     (add-hook 'mesta-mode-hook
+     (add-hook 'imli-mode-hook
           (lambda ()
             (setq fill-column 100)
             (add-hook 'before-save-hook
@@ -10,29 +10,29 @@
                       nil
                       t)))))
 
-;; Mesta error regex
-(defconst mesta-output-regexps
+;; Imli error regex
+(defconst imli-output-regexps
   '((file . "\\(.*\\)?")
     (line-column . "\\([[:digit:]]+\\)\.\\([[:digit:]]+\\)")))
 
-(defconst mesta-compilation-regexp
+(defconst imli-compilation-regexp
   (let ((re (concat "^\\(?:error\\|\\(warning\\)\\|\\(note\\)\\):[^\0]+?--> "
-                   (alist-get 'file mesta-output-regexps)
+                   (alist-get 'file imli-output-regexps)
                    " "
-                   (alist-get 'line-column mesta-output-regexps)
+                   (alist-get 'line-column imli-output-regexps)
                    "-"
-                   (alist-get 'line-column mesta-output-regexps))))
+                   (alist-get 'line-column imli-output-regexps))))
     (cons re '(3 (4 . 6) (5 . 7) (1 . 2) 3))))
 
 ;; adapted from rust-mode.el:rustc-scroll-down-after-next-error
-(defun mesta-scroll-down-after-next-error ()
+(defun imli-scroll-down-after-next-error ()
   "In the new style error messages, the regular expression
 matches on the file name (which appears after `-->`), but the
 start of the error appears a few lines earlier.  This hook runs
 after `next-error' (\\[next-error]); it simply scrolls down a few lines in
 the compilation window until the top of the error is visible."
   (save-selected-window
-    (when (eq major-mode 'sml-mode)
+    (when (eq major-mode 'imli-mode)
       (select-window (get-buffer-window next-error-last-buffer 'visible))
       (when (save-excursion
               (beginning-of-line)
@@ -47,8 +47,8 @@ the compilation window until the top of the error is visible."
 
 (eval-after-load 'compile
   '(progn
-     (add-to-list 'compilation-error-regexp-alist mesta-compilation-regexp)
-     (add-hook 'next-error-hook 'mesta-scroll-down-after-next-error)))
+     (add-to-list 'compilation-error-regexp-alist imli-compilation-regexp)
+     (add-hook 'next-error-hook 'imli-scroll-down-after-next-error)))
 
 ;; MLton pacakges
 (require 'esml-mlb-mode)
