079b41fbebc098d5dc37e790ef4b7b1dace9a207
Ram Raghunathan <rrrghun@twitch.tv>
2023-03-02 11:27:31 -0800

    50-smart-compile.el: Remove.

--- init-files/50-smart-compile.el
+++
@@ -1,7 +0,0 @@
-;;
-;; smart-compile
-;;
-
-(with-eval-after-load 'smart-compile
-  ;; Brazil CDK projects, kind of
-  (add-to-list 'smart-compile-build-system-alist '("\\`cdk.json\\'" . "brazil-build release")))
