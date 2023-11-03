194e7b95c43cd1baa9e2cb81107b5c0a16772801
Ram Raghunathan <rrrghun@twitch.tv>
2023-11-03 13:24:40 -0700

    init: Disable cua-mode
    I don't use it and it messes with my emacs memory.

--- init.el
+++ init.el
@@ -152,9 +152,6 @@ If the new path's directories does not exist, create them."
 (blink-cursor-mode -1)                                ; Steady cursor
 (pixel-scroll-precision-mode)                         ; Smooth scrolling
 
-;; Use common keystrokes by default
-(cua-mode)
-
 ;; Display line numbers in programming mode
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
 (setq-default display-line-numbers-width 3)           ; Set a minimum width
